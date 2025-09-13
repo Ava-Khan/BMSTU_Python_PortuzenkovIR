5289317bf5e814919626df1dd53186e17b178794
Ilyas <1212ananas1212@mail.ru>
2025-09-13 10:13:27 +0300

    добавление папки

---
+++ Lab1+/ex1+.py
@@ -0,0 +1,7 @@
+import math
+
+a = float(input("Введите значение угла в радианах "))
+print("Результаты функций")
+print(1-0.25*math.sin(2*a)**2+math.cos(2*a))
+print(math.cos(a)**2+math.cos(a)**4)
+
