fdd7e4d3d23faa505171dec3583090f9309e9349
Ilyas <1212ananas1212@mail.ru>
2025-09-13 10:32:54 +0300

    Лабараторная работа 2

--- labaratory_work_02/lab2.py
+++ labaratory_work_02/lab2.py
@@ -1,5 +1,14 @@
-x = int(input("Введите x "))
+import math
 
-if x <= -5:
-    print(x)
-elif
+x = float(input("Введите x от -4 до 10"))
+
+if x <= -2:
+    print("X = ",x, "Y = ",x + 3)
+elif (-2 < x <= 4):
+    print("X = ",x, "Y = ",-0.5*x)
+elif (4 < x <= 6):
+    print("X = ",x, "Y = ",-2)
+elif (6 < x <= 10):
+    print("X = ",x, "Y = ",math.sqrt((4 - (x-8)**2))-2)
+else:
+    print("Вы ввели значение из неправильного диапазона")
