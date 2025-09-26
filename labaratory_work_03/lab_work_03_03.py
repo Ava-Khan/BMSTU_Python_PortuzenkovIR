5b195e84b1727540ff00f8daa2d4cd4f11687ae6
Ilyas <1212ananas1212@mail.ru>
2025-09-27 02:53:51 +0300

    27.09.25 2:00

---
+++ labaratory_work_03/lab_work_03_03.py
@@ -0,0 +1,24 @@
+from math import *
+
+def ln(x, eps):
+    step = 1000000
+    n = 0
+    val = 0
+    while step >= eps:
+        step = ((x**(2*n+1))/(2*n+1))
+        val += step
+        n += 1
+    print(f"I   {x: 5.2f}   I   {val: 5.2f}  I  {n}  I")
+print("+--------+--------+-----+")
+print("I  X    I   Y    I   N I")
+
+
+x_st = float(input("Введите точку начада из интервала (-1; 1)"))
+x_end = float(input("Введите точку конца из интервала (-1; 1)"))
+step = float(input("Введите шаг"))
+eps = float(input("Введите значение ε погрешности"))
+
+while x_st <= x_end:
+    ln(x_st, eps)
+    x_st += step
+
